e0c0cb64a70a8acc333c4e84c0ee238ba3de5cae
Braeden <braedenleefoster@gmail.com>
2016-06-27 14:14:47 +1200

    added logo into src

--- public/app/main/rmMainCtrl.js
+++ public/app/main/rmMainCtrl.js
@@ -1,13 +1,29 @@
-angular.module('app').controller('rmMainCtrl', function($scope) {
+angular.module('app').controller('rmMainCtrl', function($scope, $http, leafletData) {
 
     function init() {
+
+        $http.get("/test.geojson").success(function(data, status) {
+            angular.extend($scope, {
+                geojson: {
+                    data: data,
+                    style: {
+                        fillColor: "green",
+                        weight: 2,
+                        opacity: 1,
+                        color: 'white',
+                        dashArray: '3',
+                        fillOpacity: 0.7
+                    }
+                }
+            })
+        });
+
         //place the map center to be first vlog position
         $scope.center = {
-            lat: 41.234024,
-            lng: -73.799021,
+            lat: 41.22503,
+            lng: -73.7948,
             zoom: 16
         };
-        
         //define mapbox as the map
         $scope.layers = {
             baselayers: {
