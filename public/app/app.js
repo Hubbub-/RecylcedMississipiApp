b1e65ac40497cc27c0bba0f168f3a6e6439e747c
Reuben Poharama <reuben.hubbubstudios.com>
2016-07-15 00:29:22 +1200

    Fixed routing for real this time

--- public/app/app.js
+++ public/app/app.js
@@ -2,10 +2,10 @@ angular.module('app', ['ngResource', 'ngRoute', 'leaflet-directive']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-    $routeProvider
-        .when('/', { templateUrl: '/partials/main/main', controller: 'rmMainCtrl'})
-        .when('/ABOUT', {templateUrl: 'partials/main/ABOUT'})
-        .when('/PRESS', {templateUrl: 'partials/main/PRESS'})
+    $routeProvider.
+        when('/', { templateUrl: '/partials/main/main', controller: 'rmMainCtrl'}).
+        when('/ABOUT', {templateUrl: 'partials/main/ABOUT'}).
+        when('/PRESS', {templateUrl: 'partials/main/PRESS'});
 
     
 });
