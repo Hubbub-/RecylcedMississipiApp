5198d1f8bae8f4abbb40ce10f3682a5e6ddb0c28
Reuben Poharama <reuben.hubbubstudios.com>
2016-08-25 10:10:46 +1200

    Added vlog #24, put header in index.jade

--- public/app/app.js
+++ public/app/app.js
@@ -3,9 +3,9 @@ angular.module('app', ['ngResource', 'ngRoute', 'leaflet-directive']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider.
-        when('/', { templateUrl: '/partials/main/main', controller: 'rmMainCtrl'}).
-        when('/ABOUT', {templateUrl: 'partials/main/ABOUT'}).
-        when('/PRESS', {templateUrl: 'partials/main/PRESS'});
+        when('/', { templateUrl: '/partials/main/main', controller: 'rmMainCtrl', title: 'expedition'}).
+        when('/ABOUT', {templateUrl: 'partials/main/ABOUT', title: 'about'}).
+        when('/PRESS', {templateUrl: 'partials/main/PRESS', title: 'press'});
 
     
 });
